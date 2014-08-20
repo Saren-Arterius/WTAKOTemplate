18c1c792834f7a6cec5060a0e0176bd3b150313c
Saren <ykmran@gmail.com>
2014-08-21 04:32:25 +0800

    Fix get long value

--- src/main/java/net/wtako/WTAKOTemplate/Utils/Config.java
+++ src/main/java/net/wtako/WTAKOTemplate/Utils/Config.java
@@ -45,7 +45,7 @@ public enum Config {
     }
 
     public long getLong() {
-        return (long) value;
+        return Integer.valueOf(getInt()).longValue();
     }
 
     public double getDouble() {
